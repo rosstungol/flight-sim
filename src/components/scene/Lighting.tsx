dccfadff26cb0ba299a91529c94dcbaeb952ede1
Ross Tungol <rossjstungol@gmail.com>
2026-05-28 23:49:21 +0800

    updates light position

--- src/components/scene/Lighting.tsx
+++ src/components/scene/Lighting.tsx
@@ -2,19 +2,14 @@ export function Lighting() {
 	return (
 		<>
 			<directionalLight
-				color={'#81AFEA'}
-				intensity={10}
-				position={[0, -15, 0]}
-			/>
-			<directionalLight
 				color={'#F1F9F0'}
 				intensity={20}
-				position={[-250, -180, -250]}
+				position={[-50, -30, -200]}
 			/>
 			<directionalLight
 				color={'#D5B9D5'}
 				intensity={10}
-				position={[-500, 15, 285]}
+				position={[-80, 50, 75]}
 			/>
 		</>
 	)
